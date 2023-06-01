0eb1f74024b547db2447a9badccf312cb9099da7
IaroslavLasiichuk <84236230+IaroslavLasiichuk@users.noreply.github.com>
2023-05-31 20:54:13 -0600

    Adds menu component to application

--- client/src/index.js
+++ client/src/index.js
@@ -2,16 +2,24 @@ import { ColorModeScript } from '@chakra-ui/react';
 import React, { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import {
+  ChakraProvider,
+  theme
+} from '@chakra-ui/react';
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 
 root.render(
-  <StrictMode>
+  <React.StrictMode>
+    <ChakraProvider theme={theme}>
     <ColorModeScript />
-    <App />
-  </StrictMode>
+    <Router>
+      <Routes>
+        <Route path='/*' element={<App />}></Route>
+      </Routes>
+      </Router>
+      </ChakraProvider>
+  </React.StrictMode>
 );
-
-serviceWorker.unregister();
