38ff9d22a516c8859c94534658e63224980ff4f0
IaroslavLasiichuk <84236230+IaroslavLasiichuk@users.noreply.github.com>
2023-06-04 11:07:49 -0600

    Cors

--- server/server.js
+++ server/server.js
@@ -4,8 +4,6 @@ const cors = require('cors');
 require("dotenv").config();
 const { authMiddleware } = require('./utils/auth');
 
-app.use(cors());
-
 // Import the ApolloServer class
 const { typeDefs, resolvers } = require("./schemas");
 const { ApolloServer } = require("apollo-server-express");
@@ -14,6 +12,7 @@ const db = require("./config/connection");
 
 const PORT = process.env.PORT || 4000;
 const app = express();
+app.use(cors());
 
 const server = new ApolloServer({
   typeDefs,
